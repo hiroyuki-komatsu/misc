436691494f8fdd37b5b426de02c19f06fb21369c
Hiroyuki Komatsu <hiroyuki.komatsu@gmail.com>
2017-11-25 14:40:43 +0900

    Added cifar10 tools

---
+++ apps/kingdomino/cifar10.py
@@ -0,0 +1,90 @@
+#!/usr/local/bin/python3
+
+import argparse
+import numpy as np
+import os
+from PIL import Image
+
+
+def cifar10ToImage(byte_array, width=32, height=32, depth=3):
+  label = np.frombuffer(byte_array[0:1], dtype=np.uint8)
+
+  np_array = np.frombuffer(byte_array[1:], dtype=np.uint8)
+  reshaped = np_array.reshape((depth, width, height))
+  transposed = reshaped.transpose((1, 2, 0))
+
+  return label, Image.fromarray(transposed)
+
+
+def imageToCifar10(label, image, width=32, height=32):
+  np_array = np.asarray(image.resize((width, height)))
+  transposed = np_array.transpose((2, 0, 1))
+
+  return bytes([label]) + transposed.tobytes()
+
+
+class Cifar10Data(object):
+  def __init__(self, data=None, width=32, height=32, depth=3):
+    self.width = width
+    self.height = height
+    self.depth = depth
+    self.data = data or bytes([])
+    self.count = 0
+
+  def appendImage(self, label, image):
+    self.data += imageToCifar10(label, image, self.width, self.height)
+    self.count += 1
+
+  def getImage(self, index):
+    data_size = 1 + self.width * self.height * self.depth
+    data = self.data[data_size * index : data_size * (index + 1)]
+    return cifar10ToImage(data)
+
+  def save(self, filepath):
+    with open(filepath, mode='wb') as file:
+      file.write(self.data)
+
+
+def generateData(data_dir, output_file):
+  labels = {'base': 0, 'forest': 1, 'grass': 2, 'pond': 3, 'rock': 4, 'sea': 5, 'weat': 6}
+  cifar10 = Cifar10Data()
+
+  for basedir, dirs, files in os.walk(data_dir):
+    label = labels.get(os.path.basename(basedir), -1)
+    if label == -1:
+      continue
+    for filename in files:
+      if not filename.endswith('.jpg'):
+        continue
+      image = Image.open(os.path.join(basedir, filename))
+      cifar10.appendImage(label, image)
+  
+  cifar10.save(output_file)
+  print ('count: %d' % cifar10.count)
+
+
+def dumpImages():
+  datafile = 'cifar10.bin'
+  with open(datafile, mode='rb') as src:
+    cifar10 = Cifar10Data(src.read())
+
+  for index in range(2):
+    label, image = cifar10.getImage(index)
+
+    print('label: %d' % label)
+    filename = 'cifar10-%02d-%d.png' % (index, label)
+    image.save(filename, format='png')
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--datadir')
+  parser.add_argument('--output')
+  args = parser.parse_args()
+
+  # dumpImages()
+  # data_dir = 'data/output'
+  # output_file = 'cifar10.bin'
+  generateData(args.datadir, args.output)
+
+main()
---
+++ apps/kingdomino/eval_color.py
@@ -0,0 +1,252 @@
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""Evaluation for CIFAR-10.
+
+Accuracy:
+cifar10_train.py achieves 83.0% accuracy after 100K steps (256 epochs
+of data) as judged by cifar10_eval.py.
+
+Speed:
+On a single Tesla K40, cifar10_train.py processes a single batch of 128 images
+in 0.25-0.35 sec (i.e. 350 - 600 images /sec). The model reaches ~86%
+accuracy after 100K steps in 8 hours of training time.
+
+Usage:
+Please see the tutorial and website for how to download the CIFAR-10
+data set, compile the program and train the model.
+
+http://tensorflow.org/tutorials/deep_cnn/
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import numpy as np
+import tensorflow as tf
+
+# import cifar10
+
+import argparse
+parser = argparse.ArgumentParser()
+
+# Basic model parameters.
+parser.add_argument('--batch_size', type=int, default=1,
+                    help='Number of images to process in a batch.')
+
+parser.add_argument('--use_fp16', type=bool, default=False,
+                    help='Train the model using fp16.')
+
+FLAGS = parser.parse_args()
+NUM_CLASSES = 10
+MOVING_AVERAGE_DECAY = 0.9999     # The decay to use for the moving average.
+
+
+def _variable_on_cpu(name, shape, initializer):
+  """Helper to create a Variable stored on CPU memory.
+
+  Args:
+    name: name of the variable
+    shape: list of ints
+    initializer: initializer for Variable
+
+  Returns:
+    Variable Tensor
+  """
+  with tf.device('/cpu:0'):
+    dtype = tf.float16 if FLAGS.use_fp16 else tf.float32
+    var = tf.get_variable(name, shape, initializer=initializer, dtype=dtype)
+  return var
+
+
+def _variable_with_weight_decay(name, shape, stddev, wd):
+  """Helper to create an initialized Variable with weight decay.
+
+  Note that the Variable is initialized with a truncated normal distribution.
+  A weight decay is added only if one is specified.
+
+  Args:
+    name: name of the variable
+    shape: list of ints
+    stddev: standard deviation of a truncated Gaussian
+    wd: add L2Loss weight decay multiplied by this float. If None, weight
+        decay is not added for this Variable.
+
+  Returns:
+    Variable Tensor
+  """
+  dtype = tf.float16 if FLAGS.use_fp16 else tf.float32
+  var = _variable_on_cpu(
+      name,
+      shape,
+      tf.truncated_normal_initializer(stddev=stddev, dtype=dtype))
+  if wd is not None:
+    weight_decay = tf.multiply(tf.nn.l2_loss(var), wd, name='weight_loss')
+    tf.add_to_collection('losses', weight_decay)
+  return var
+
+
+def inference(images):
+  """Build the CIFAR-10 model.
+
+  Args:
+    images: Images returned from distorted_inputs() or inputs().
+
+  Returns:
+    Logits.
+  """
+  # We instantiate all variables using tf.get_variable() instead of
+  # tf.Variable() in order to share variables across multiple GPU training runs.
+  # If we only ran this model on a single GPU, we could simplify this function
+  # by replacing all instances of tf.get_variable() with tf.Variable().
+  #
+  # conv1
+  with tf.variable_scope('conv1') as scope:
+    kernel = _variable_with_weight_decay('weights',
+                                         shape=[5, 5, 3, 64],
+                                         stddev=5e-2,
+                                         wd=0.0)
+    conv = tf.nn.conv2d(images, kernel, [1, 1, 1, 1], padding='SAME')
+    biases = _variable_on_cpu('biases', [64], tf.constant_initializer(0.0))
+    pre_activation = tf.nn.bias_add(conv, biases)
+    conv1 = tf.nn.relu(pre_activation, name=scope.name)
+
+  # pool1
+  pool1 = tf.nn.max_pool(conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1],
+                         padding='SAME', name='pool1')
+  # norm1
+  norm1 = tf.nn.lrn(pool1, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,
+                    name='norm1')
+
+  # conv2
+  with tf.variable_scope('conv2') as scope:
+    kernel = _variable_with_weight_decay('weights',
+                                         shape=[5, 5, 64, 64],
+                                         stddev=5e-2,
+                                         wd=0.0)
+    conv = tf.nn.conv2d(norm1, kernel, [1, 1, 1, 1], padding='SAME')
+    biases = _variable_on_cpu('biases', [64], tf.constant_initializer(0.1))
+    pre_activation = tf.nn.bias_add(conv, biases)
+    conv2 = tf.nn.relu(pre_activation, name=scope.name)
+
+  # norm2
+  norm2 = tf.nn.lrn(conv2, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,
+                    name='norm2')
+  # pool2
+  pool2 = tf.nn.max_pool(norm2, ksize=[1, 3, 3, 1],
+                         strides=[1, 2, 2, 1], padding='SAME', name='pool2')
+
+  # local3
+  with tf.variable_scope('local3') as scope:
+    # Move everything into depth so we can perform a single matrix multiply.
+    reshape = tf.reshape(pool2, [FLAGS.batch_size, -1])
+    dim = reshape.get_shape()[1].value
+    weights = _variable_with_weight_decay('weights', shape=[dim, 384],
+                                          stddev=0.04, wd=0.004)
+    biases = _variable_on_cpu('biases', [384], tf.constant_initializer(0.1))
+    local3 = tf.nn.relu(tf.matmul(reshape, weights) + biases, name=scope.name)
+
+  # local4
+  with tf.variable_scope('local4') as scope:
+    weights = _variable_with_weight_decay('weights', shape=[384, 192],
+                                          stddev=0.04, wd=0.004)
+    biases = _variable_on_cpu('biases', [192], tf.constant_initializer(0.1))
+    local4 = tf.nn.relu(tf.matmul(local3, weights) + biases, name=scope.name)
+
+  # linear layer(WX + b),
+  # We don't apply softmax here because
+  # tf.nn.sparse_softmax_cross_entropy_with_logits accepts the unscaled logits
+  # and performs the softmax internally for efficiency.
+  with tf.variable_scope('softmax_linear') as scope:
+    weights = _variable_with_weight_decay('weights', [192, NUM_CLASSES],
+                                          stddev=1/192.0, wd=0.0)
+    biases = _variable_on_cpu('biases', [NUM_CLASSES],
+                              tf.constant_initializer(0.0))
+    softmax_linear = tf.add(tf.matmul(local4, weights), biases, name=scope.name)
+
+  return softmax_linear
+
+# Process images of this size. Note that this differs from the original CIFAR
+# image size of 32 x 32. If one alters this number, then the entire model
+# architecture will change and any model would need to be retrained.
+IMAGE_SIZE = 24
+
+def input(filename):
+  if not tf.gfile.Exists(filename):
+    raise ValueError('Failed to find file: ' + filename)
+
+  jpeg_r = tf.read_file(filename)
+  read_input = tf.image.decode_jpeg(jpeg_r, channels=3)
+
+  reshaped_image = tf.cast(read_input, tf.float32)
+
+  height = IMAGE_SIZE
+  width = IMAGE_SIZE
+
+  # Image processing for evaluation.
+  # Crop the central [height, width] of the image.
+  resized_image = tf.image.resize_image_with_crop_or_pad(reshaped_image,
+                                                         height, width)
+
+  # Subtract off the mean and divide by the variance of the pixels.
+  float_image = tf.image.per_image_standardization(resized_image)
+
+  # Set the shapes of tensors.
+  float_image.set_shape([height, width, 3])
+  return tf.reshape(float_image, [-1, height, width, 3])
+
+
+def evaluate():
+  """Eval CIFAR-10 for a number of steps."""
+  # Get images for CIFAR-10.
+  data_file = '/tmp/output_0013.jpg'
+  images = input(data_file)
+
+  # Build a Graph that computes the logits predictions from the
+  # inference model.
+  logits = inference(images)
+
+  # Calculate predictions.
+  top_k_op = tf.nn.top_k(logits, 1)
+  values_op, indices_op = top_k_op
+
+  # Restore the moving average version of the learned variables for eval.
+  variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY)
+  variables_to_restore = variable_averages.variables_to_restore()
+  saver = tf.train.Saver(variables_to_restore)
+
+  checkpoint_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'train/model')
+  print(checkpoint_dir)
+  with tf.Session() as sess:
+    ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+    if ckpt and ckpt.model_checkpoint_path:
+      # Restores from checkpoint
+      saver.restore(sess, ckpt.model_checkpoint_path)
+    else:
+      print('No checkpoint file found')
+      return
+
+    predictions = sess.run(indices_op)
+    print(predictions)
+    return
+
+
+def main(argv=None):  # pylint: disable=unused-argument
+  evaluate()
+
+
+if __name__ == '__main__':
+  tf.app.run()
