d8ec1630ba9141b64532c94c7724abc3851f67fb
Hiroyuki Komatsu <hiroyuki.komatsu@gmail.com>
2021-05-04 20:01:52 +0900

    Update ac_instagram.

--- apps/ac_instagram/userdata_template.py
+++ apps/ac_instagram/userdata_template.py
@@ -1,5 +1,6 @@
 # Please fill the values and rename this file to userdata.py
 
+USER_AGENT = None  # e.g. 'Mozilla/5.0 ...'
 INSTAGRAM_USERNAME = ''   # e.g. 'sampleuser'
 SLACK_WEBHOOK = ''  # e.g. 'https://hooks.slack.com/services/...'
 SHORTCODE_FILE = '/tmp/shortcode.txt'   # Path to store the last entry.
