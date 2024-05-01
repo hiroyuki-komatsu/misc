18ebd83519080dd6ac308fd3555deaf4306349ce
Hiroyuki Komatsu <hiroyuki.komatsu@gmail.com>
2024-05-01 15:29:42 +0900

    Add onKeyInput

--- apps/hidemu/app/src/main/java/org/taiyaki/hidemu/MainActivity.kt
+++ apps/hidemu/app/src/main/java/org/taiyaki/hidemu/MainActivity.kt
@@ -1,5 +1,7 @@
 package org.taiyaki.hidemu
 
+import android.content.Context
+import android.hardware.usb.UsbManager
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -15,9 +17,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import com.hoho.android.usbserial.driver.UsbSerialDriver
+import com.hoho.android.usbserial.driver.UsbSerialPort
+import com.hoho.android.usbserial.driver.UsbSerialProber
 import org.taiyaki.hidemu.ui.theme.HIDEmulatorTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var manager : UsbManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -25,20 +32,45 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(),
                         color = MaterialTheme.colorScheme.background) {
-                    Keyboard()
+                    Keyboard(onKeyInput = ::onKeyInput)
+                    // Keyboard(onKeyInput = { Log.d("onKeyInput", "place holder")})
                 }
             }
         }
+        manager = getSystemService(Context.USB_SERVICE) as UsbManager
+    }
+
+    private fun onKeyInput(char : String) {
+        Log.d("onKeyInput", char)
+        val availableDrivers : List<UsbSerialDriver> =
+            UsbSerialProber.getDefaultProber().findAllDrivers(manager)
+        if (availableDrivers.isEmpty()) {
+            Log.d("onKeyInput", "no availableDrivers")
+            return
+        }
+        val driver : UsbSerialDriver = availableDrivers[0]
+        val connection = manager.openDevice(driver.device)
+        if (connection == null) {
+            Log.d("onKeyInput", "no connection")
+            return
+        }
+
+        val port = driver.ports[0] // Most devices have just one port (port 0)
+        port.open(connection)
+        port.setParameters(115200, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE)
+        port.close()
+        Log.d("onKeyInput", "done")
     }
 }
 
 @Composable
-fun Keyboard() {
+fun Keyboard(onKeyInput: (String) -> Unit) {
     val context = LocalContext.current
     Column {
         Button(onClick = {
             Toast.makeText(context, "clicked", Toast.LENGTH_SHORT).show()
             Log.d("Button", "onClick")
+            onKeyInput("a")
         }) {
             Text("Button")
         }
@@ -52,6 +84,6 @@ fun Keyboard() {
 @Composable
 fun GreetingPreview() {
     HIDEmulatorTheme {
-        Keyboard()
+        Keyboard(onKeyInput = {})
     }
 }
